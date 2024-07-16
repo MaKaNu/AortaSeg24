3cad06586bcd0008a9613c1b72600238972618be
MaKaNu <matti.kaupenjohann@fh-dortmund.de>
2024-07-16 16:02:16 +0200

    Update Script

--- venv_install_torch.sh
+++ venv_install_torch.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 pip install torch==2.0.0 torchvision==0.15.1 --index-url https://download.pytorch.org/whl/cu117
-pip install monai[itk,nibabel,einops]==1.3.0
+DEP1=nibabel,scikit-image,scipy,pillow,tensorboard,gdown,pytorch-ignite,torchvision,itk,
+DEP2=tqdm,lmdb,psutil,cucim,openslide-python,pandas,einops,transformers,
+DEP3=mlflow,clearml,matplotlib,tensorboardX,tifffile,imagecodecs,pyyaml,jsonschema,ninja,
+DEP4=pynrrd,pydicom,h5py,nni,optuna,onnx,onnxruntime,zarr,lpips,nvidia-ml-py,huggingface_hub,pyamg
+pip install monai["$DEP1$DEP2$DEP3$DEP4"]==1.3.0
